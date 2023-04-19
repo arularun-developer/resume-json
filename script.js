8d1e4aaca9006453afb1f584915dd23084adda76
arularun-developer <129672765+arularun-developer@users.noreply.github.com>
2023-04-19 18:13:27 +0530

    json-format

---
+++ script.js
@@ -0,0 +1,56 @@
+ let myResume={
+    "basics":{
+        "name":"S.ARUNKUMAR",
+        "email":"arularunoffical1110@gmail.com",
+        "phone":9597572147,
+        "degree":"B.E",
+        "location":{
+            "address":"12/65 south street keelaurappanur thirumangalam",
+            "postalcode":625706,
+            "city":"madurai",
+            "state":"Tamilnadu",
+            "country":"India",
+        },
+        "profiles":[
+            {
+                "github":"https://github.com/arularun-developer"
+            }
+        ]
+},
+"work":[
+    {
+        "company":"ADR plastic PVT",
+        "position":"plant supervisor",
+        "startdate":"2021-01-01",
+        "enddate":"2023-04-01",
+        "summary":" i am the plant supervisor handing various problem and gaining lots of experience",
+    },
+],
+ "education":[
+    {
+        "institution":"RATHINAM TECHNICAL CAMPUS",
+        "department":"mechanical",
+        "studyType":"fulltime",
+        "batch start year":"2016",
+        "batch end year":"2020",
+        "CGPA":"6.78",
+    }
+ ],
+ "skills":[
+    {
+        "skills":"HTML,CSS,JAVASCRIPT",
+        "level":"beginner",
+    }
+ ],
+ "languages":[
+    {
+        "language":"Tamil,English",
+    }
+ ],
+ "interests":[
+    {
+      "interests":"Photography,Design"
+    }
+ ]
+}
+console.log(myResume)
